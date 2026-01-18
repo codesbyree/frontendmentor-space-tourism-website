a203e63d93a63f181a382796cc9dd8f51ef60c5c
codesbyree <bahreesaepul1@gmail.com>
2026-01-18 22:05:12 +0700

    style: enhance layout and responsiveness of destination selector and page

--- src/components/DestinationSelector.tsx
+++ src/components/DestinationSelector.tsx
@@ -89,7 +89,7 @@ export default function DestinationSelector() {
     return (
        <>
         <div className="py-8 flex justify-center items-center">
-            <div className='w-40 h-40 relative'>
+            <div className='w-40 h-40 md:w-80 md:h-80 relative'>
             {destinationImage.map(dest => (
                 <img key={dest.title} className={
                 clsx("absolute top-0 left-0 w-full h-full transition-all duration-300", selectedDestinations === dest.title ? 'opacity-100 rotate-0' : 'opacity-0 rotate-6')} src={dest.png} srcSet={dest.webp} />
@@ -98,9 +98,9 @@ export default function DestinationSelector() {
 
         </div>
 
-        <div className='flex justify-between px-8'>
+        <div className='flex justify-center gap-8 px-8'>
             {destinationList.map(dest => (
-                <button onClick={() => setSelectedDestinations(dest)} key={dest} className={clsx('font-sans-codensed text-sm tracking-wider uppercase transition-colors duration-300 relative', selectedDestinations === dest ? 'text-white' : 'text-blue-300')}>
+                <button onClick={() => setSelectedDestinations(dest)} key={dest} className={clsx('font-sans-codensed text-sm tracking-wider uppercase transition-colors duration-300 relative md:text-base md:tracking-widest', selectedDestinations === dest ? 'text-white' : 'text-blue-300')}>
                     <span>{dest}</span>
                     <span className={clsx('absolute left-0 w-full h-[3px] bg-white transition-all duration-200', selectedDestinations === dest ? 'opacity-100 -bottom-2' : 'opacity-0 bottom-0')}></span>
                 </button>
@@ -108,9 +108,9 @@ export default function DestinationSelector() {
         </div>
 
         <div className='flex flex-col gap-4 mt-4'>
-            <h2 className='uppercase text-white font-serif text-center text-6xl'>{selectedDestinations}</h2>
+            <h2 className='uppercase text-white font-serif text-center text-6xl md:text-8xl'>{selectedDestinations}</h2>
             
-            <div className='min-h-36 relative'>
+            <div className='min-h-36 md:min-h-20 relative'>
                 {destinationDescription.map(desc => (
                     <p key={desc.title} className={clsx('text-base absolute top-0 self-center text-blue-300 leading-7 font-sans text-center transition-all duration-500', selectedDestinations === desc.title ? 'opacity-100' : 'opacity-0')}>{desc.description}</p>
                 ))}
@@ -119,7 +119,7 @@ export default function DestinationSelector() {
 
         <span className='w-full h-[1px] bg-white/25'></span>
 
-        <div className='flex flex-col gap-6'>
+        <div className='flex flex-col gap-6 md:flex-row'>
             <div className='w-full flex flex-col gap-3 overflow-hidden'>
                 <h2 className='font-sans-codensed tracking-wider text-blue-300 text-sm text-center'>AVG. DISTANCE</h2>
 
@@ -135,7 +135,7 @@ export default function DestinationSelector() {
                 </div>
             </div>
 
-            <div>
+            <div className='w-full flex flex-col gap-3 overflow-hidden'>
                 <h2 className='font-sans-codensed tracking-wider text-blue-300 text-sm text-center'>EST. TRAVEL TIME</h2>
                 
                 <div className={clsx('w-full flex flex-row transition-transform duration-300',
